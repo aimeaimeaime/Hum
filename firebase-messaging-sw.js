ef71586a452783551e9ba519af574f74d2a429ae
aimeaimeaime <babineaime2004@gmail.com>
2025-05-05 20:28:27 +0100

    Correction oooooou mise à jr du backend

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,8 +1,6 @@
-// Import des scripts Firebase
-importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js");
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 
-// Configuration Firebase
 firebase.initializeApp({
   apiKey: "AIzaSyAb8gGl4qBZVtBqYS4--XkmeGPCZy8M25Y",
   authDomain: "my-noti-16c94.firebaseapp.com",
@@ -12,10 +10,8 @@ firebase.initializeApp({
   appId: "1:1062983320683:web:da32270bb7eea4524a5d2e"
 });
 
-// Initialisation du service messaging
 const messaging = firebase.messaging();
 
-// Gestion de la réception des notifications en arrière-plan
 messaging.setBackgroundMessageHandler(function(payload) {
   console.log('[firebase-messaging-sw.js] Message reçu en arrière-plan : ', payload);
 
