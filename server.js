6a049cdf9bb521b4e64a16bb5580f000a30a80b5
aimeaimeaime <babineaime2004@gmail.com>
2025-05-06 01:05:41 +0100

    Initial commivvvvvt

--- server.js
+++ server.js
@@ -185,15 +185,28 @@
 
 
 
+
+
 const express = require('express');
 const admin = require('firebase-admin');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
 
-// 🔐 Charge le fichier JSON (ne jamais le push sur GitHub)
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
-
+// 🔐 Création manuelle de l'objet serviceAccount à partir des variables Render
+const serviceAccount = {
+  type: 'service_account',
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+  token_uri: 'https://oauth2.googleapis.com/token',
+  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+  universe_domain: 'googleapis.com'
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
